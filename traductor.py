3674be7d131be4cecdfe44f71543ff467bdcd520
the0rem <shaun.smekel@theorem.net.au>
2015-09-19 16:05:57 +1000

    Created list of supported docker-compose functions
    Signed-off-by: the0rem <shaun.smekel@theorem.net.au>

--- traductor.py
+++ traductor.py
@@ -5,3 +5,41 @@ from jinja2 import Template
 
 from . import translators
 
+
+# List of supported docker-compose commands
+OPTIONS_CAPABILITIES = [
+    "cap_add",
+    "cap_drop",
+    "command",
+    "container_name",
+    "cpu_shares",
+    "cpuset",
+    "devices",
+    "domainname",
+    "dns",
+    "dns_search",
+    "entrypoint",
+    "env_file",
+    "environment",
+    "expose",
+    "hostname",
+    "links",
+    "log_driver",
+    "mac_address",
+    "mem_limit",
+    "memswap_limit",
+    "net",
+    "pid",
+    "ports",
+    "privileged",
+    "read_only",
+    "restart",
+    "stdin_open",
+    "tty",
+    "user",
+    "volume_driver",
+    "volumes",
+    "volumes_from",
+    "working_dir",
+]
+
